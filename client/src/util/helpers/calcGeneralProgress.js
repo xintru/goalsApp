a7e7c4b95337b657e6707f888737a050a6f79085
xintru <xintru@gmail.com>
2020-05-24 20:36:20 +0700

    goal progress fix

--- client/src/util/helpers/calcGeneralProgress.js
+++ client/src/util/helpers/calcGeneralProgress.js
@@ -1,9 +1,11 @@
 const calcGeneralProgress = (goals) =>
-  goals
+  (goals
     .map((goal) => {
       const completedNum = goal.subgoals.reduce((a, b) => a + b.completed, 0)
-      return +(completedNum / goal.subgoals.length).toFixed(2)
+      return +(completedNum / (goal.subgoals.length || 1)).toFixed(2)
     })
-    .reduce((a, b) => a + b, 0) / goals.length
+    .reduce((a, b) => a + b, 0) /
+    goals.length) *
+  100
 
 export default calcGeneralProgress
--- client/src/util/helpers/calcGoalProgress.js
+++ client/src/util/helpers/calcGoalProgress.js
@@ -1,6 +1,6 @@
 const calcGoalProgress = (goal) =>
   +(
-    goal.subgoals.reduce((a, b) => a + b.completed, 0) / goal.subgoals.length
+    goal.subgoals.reduce((a, b) => a + b.completed, 0) / (goal.subgoals.length || 1)
   ).toFixed(2) * 100
 
 export default calcGoalProgress
